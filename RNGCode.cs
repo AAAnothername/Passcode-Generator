ce9f31d7a229708335eb72fae70d650f82027e64
AAAnothername <103200793+AAAnothername@users.noreply.github.com>
2022-04-07 20:33:25 +0200

    Shutting down...
    Added the ability to shut down program, and ensured it doesn't let you input random stuff while shutting down. Yay.
    Also added the ability to confirm.

--- RNGCode.cs
+++ RNGCode.cs
@@ -1,26 +1,57 @@
 //forgive me for how shitty this code is
 using System;
 using System.Linq;
-                    
+using System.Threading;
+using System.Windows.Forms;
+
 public class Program
 {
     public static void Main()
     {
+        bool dying = false;
         int[] numbers = { };
         Console.WriteLine("Press Enter to generate your code...");
         Console.WriteLine("Press PageUp or PageDown to save your code to a text file...");
+        Console.WriteLine("Press Escape to exit...");
         Console.WriteLine();
-        _Generate:
+    _Generate:
         ConsoleKeyInfo result = Console.ReadKey();
-        if ((result.KeyChar == 13))
+        if ((result.KeyChar == 13) && (dying == false))
         {
 
         }
         ////THIS CODE IS UNSTABLE. ITS INTENDED TO WORK WHEN PRESSING PAGEUP OR PAGEDOWN, WHY THE FUCK WONT IT WORK
-        //else if ((result.KeyChar == 33) || (result.KeyChar == 34))
+        //else if ((result.KeyChar == 33) || (result.KeyChar == 34) && (dying == false)
         //{
         //
         //}
+        else if ((result.KeyChar == 27) && (dying == false))
+        {
+            Console.WriteLine();
+            Console.WriteLine("Are you sure? Press Escape again to confirm.");
+            Console.WriteLine();
+            ConsoleKeyInfo confirmation = Console.ReadKey();
+            if ((confirmation.KeyChar == 27))
+            {
+                dying = true;
+                Console.WriteLine();
+                Console.WriteLine("Shutting down...");
+                Thread.Sleep(3000);
+                System.Environment.Exit(0);
+                Console.WriteLine("Shutdown failed, please try again.");
+                goto _Generate;
+                //might be archaic, but it works
+                //it tells the program that its shutting off ("dying"), logs it, waits three seconds (for realism) and shuts down.
+                //If it fails to shut down, it will log and let you input
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Shutdown cancelled.");
+                Console.WriteLine();
+                goto _Generate;
+            }
+        }
         else
         {
             Console.WriteLine();
@@ -36,7 +67,7 @@ public class Program
         bool taken = false;
         Console.WriteLine("Generated Code:");
         Random r = new Random(Guid.NewGuid().GetHashCode());
-        x = r.Next(111111, 999999); //YOU CAN EDIT THIS TO INCREASE AMOUNT OF CODES
+        x = r.Next(111111111, 999999999); //YOU CAN EDIT THIS TO INCREASE AMOUNT OF CODES
 
         if (numbers.Contains(x)) //trying to test if a number is taken in the array
         { taken = true; }
